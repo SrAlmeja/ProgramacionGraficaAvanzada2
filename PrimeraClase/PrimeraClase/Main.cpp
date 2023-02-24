dc464eedbfd142f0ada6691f27dc511dd9c8dac0
Daniel Eduardo Navarrete Flores <daniel_navarrete_05@hotmail.com>
2023-02-23 19:03:13 -0600

    Trifuerza

--- PrimeraClase/PrimeraClase/Main.cpp
+++ PrimeraClase/PrimeraClase/Main.cpp
@@ -13,7 +13,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"    FragColor = vec4(0.9922f, 0.9052f, 0.298f, 1.0f);\n"
 "}\n\0";
 
 int main()
@@ -26,11 +26,21 @@ int main()
     //apuntador de refrencia a la ventana que se usara en la gpu
     GLFWwindow* window = glfwCreateWindow(800, 800, "test", NULL, NULL);
 
-    GLfloat vertices[]
+    GLfloat vertices[] =
     {
-        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+         -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,       // Esquina inferior izq
+         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,        // Esquina inferior derecha
+         0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   // Esquina superior
+         -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,      // Interior izquierda
+         0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,    // Interior derecha
+         0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f        // Interior abajo
+    };
+
+    GLuint indices[] =
+    {
+    0, 3, 5, // Triangulo inferior izq
+    3, 2, 4, // Triangulo inferior der
+    5, 4, 1 // Triangulo superior
     };
 
     glfwMakeContextCurrent(window);
@@ -57,33 +67,40 @@ int main()
 
     //se almacena y renderiza la infromacion en la gpu
     GLuint VAO, VBO;
+    GLuint EBO;
+
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 
     glViewport(0, 0, 800, 800);
-    glClearColor(0.8f, 0.6f, 0.2f, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClearColor(0, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         glfwPollEvents();
@@ -92,6 +109,7 @@ int main()
     //limpia el buffer para liberar la gpu
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
