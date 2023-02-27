4b766dba13ec5c69bba6c528c50d511ebf04ba3f
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2023-02-26 22:45:31 -0600

    Trifuerza con bordes

--- PrimeraClase/PrimeraClase/Main.cpp
+++ PrimeraClase/PrimeraClase/Main.cpp
@@ -9,7 +9,21 @@ const char* vertexShaderSource = "#version 330 core\n"
 "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
+const char* insideVertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos; \n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
 const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);\n"
+"}\n\0";
+
+const char* insideFragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
@@ -36,11 +50,34 @@ int main()
          0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f        // Interior abajo
     };
 
+    GLfloat insideVertex[] =
+    {
+         -0.45f, -0.45f * float(sqrt(3)) / 3, 0.0f,       // Esquina inferior izq
+         -0.5f / 2, 0.5f * float(sqrt(1.5f)) / 6, 0.0f,      // Interior izquierda
+         -0.05f, -0.45f * float(sqrt(3)) / 3, 0.0f,        // Interior abajo
+
+
+         0.05f, -0.45f * float(sqrt(3)) / 3, 0.0f,        // Interior abajo
+         0.45f, -0.45f * float(sqrt(3)) / 3, 0.0f,        // Esquina inferior derecha
+         0.5f / 2, 0.5f * float(sqrt(1.5f)) / 6, 0.0f,   // Esquina superior
+
+         -0.4f / 2, 0.6f * float(sqrt(3)) / 6, 0.0f,      // Interior izquierda
+         0.0f, 0.45f * float(sqrt(3)) * 2 / 3, 0.0f,   // Esquina superior
+         0.4f / 2, 0.6f * float(sqrt(3)) / 6, 0.0f,    // Interior derecha
+    };
+
     GLuint indices[] =
     {
     0, 3, 5, // Triangulo inferior izq
     3, 2, 4, // Triangulo inferior der
     5, 4, 1 // Triangulo superior
+    };
+
+    GLuint insideIndex[] =
+    {
+    0, 1, 2, // Triangulo inferior izq
+    3, 4, 5, // Triangulo inferior der
+    6, 7, 8 // Triangulo superior
     };
 
     glfwMakeContextCurrent(window);
@@ -51,9 +88,17 @@ int main()
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
+    GLuint insideVertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(insideVertexShader, 1, &insideVertexShaderSource, NULL);
+    glCompileShader(insideVertexShader);
+
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+
+    GLuint insideFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(insideFragmentShader, 1, &insideFragmentShaderSource, NULL);
+    glCompileShader(insideFragmentShader);
 
     //se crea un programa donde se guardan los shaders
     GLuint shaderProgram = glCreateProgram();
@@ -61,18 +106,33 @@ int main()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
+    GLuint insideShaderProgram = glCreateProgram();
+    glAttachShader(insideShaderProgram, insideVertexShader);
+    glAttachShader(insideShaderProgram, insideFragmentShader);
+    glLinkProgram(insideShaderProgram);
+
     //Se eliminan los shaders
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    glDeleteShader(insideVertexShader);
+    glDeleteShader(insideFragmentShader);
+
 
     //se almacena y renderiza la infromacion en la gpu
     GLuint VAO, VBO;
     GLuint EBO;
 
+    GLuint InsideVAO, InsideVBO;
+    GLuint InsideEBO;
+
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
+
+    glGenVertexArrays(1, &InsideVAO);
+    glGenBuffers(1, &InsideVBO);
+    glGenBuffers(1, &InsideEBO);
 
     glBindVertexArray(VAO);
 
@@ -90,16 +150,36 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
+
+
+    glBindVertexArray(InsideVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, InsideVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(insideVertex), insideVertex, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, InsideEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(insideIndex), insideIndex, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
     glViewport(0, 0, 800, 800);
-    glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0, 0, 0, 0);
+        glClearColor(0.5255f, 0.8706f, 0.4471, 1);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glUseProgram(insideShaderProgram);
+        glBindVertexArray(InsideVAO);
         glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
@@ -110,7 +190,11 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+    glDeleteVertexArrays(1, &InsideVAO);
+    glDeleteBuffers(1, &InsideVBO);
+    glDeleteBuffers(1, &InsideEBO);
     glDeleteProgram(shaderProgram);
+    glDeleteProgram(insideShaderProgram);
 
     glfwTerminate();
 }
